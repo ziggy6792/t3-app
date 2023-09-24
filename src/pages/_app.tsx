1254a53d77d2e7bee0c4ff8f5a752a519590fa83
Simon Verhoeven <simon.verhoeven>
2023-09-25 01:07:57 +0800

    working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,17 @@
-import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
 
-import { api } from "~/utils/api";
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
 
-import "~/styles/globals.css";
-
-const MyApp: AppType<{ session: Session | null }> = ({
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}) => {
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-};
-
-export default api.withTRPC(MyApp);
+}
